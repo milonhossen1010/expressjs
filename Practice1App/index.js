65a8e73773f06443e7aa95c2e75eac7817b46f6c
milon <milonhossen1010@gmail.com>
2022-07-21 11:53:23 +0600

    Status code (status)

--- Practice1App/index.js
+++ Practice1App/index.js
@@ -8,6 +8,12 @@ app.get("/", function (req, res) {
 
 });
 
+app.post("/", function (req, res) {
+
+    res.status(401).send("Unauthorized code.");
+
+});
+
 
 app.listen(5050, function () {
     console.log("Express server running.");
