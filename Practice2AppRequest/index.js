5bd4dccbab4863fdfa5d538f12e907f02e2f11b9
milon <milonhossen1010@gmail.com>
2022-07-23 12:54:27 +0600

    Body-parser install and body data get (req.body)

--- Practice2AppRequest/index.js
+++ Practice2AppRequest/index.js
@@ -1,5 +1,8 @@
 let express = require("express");
+let bodyParser = require('body-parser');
+
 app=express();
+app.use(bodyParser.json());
 
 
 
@@ -10,6 +13,14 @@ app.get("/", function (req, res) {
     res.end(name);
 });
 
+//Body data get
+app.post("/home", function (req, res) {
+
+    let JSONData =  req.body;
+    let stringData = JSON.stringify(JSONData);
+
+    res.send(stringData);
+});
 
 
 app.listen(5050, function () {
